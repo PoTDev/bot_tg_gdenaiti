2cdd845ec1d44953624215ca4f58f2361e4d3b7c
PoTDeV <79372735+PoTDev@users.noreply.github.com>
2025-05-06 14:53:16 +0000

    commit user work

--- group_work/user_work.py
+++ group_work/user_work.py
@@ -1,43 +1,50 @@
+# group_work/user_work.py
 import asyncio
-
+from aiogram import Router, types, Bot
+from aiogram.types import ChatMemberUpdated
+from aiogram.filters.chat_member_updated import ChatMemberUpdatedFilter, JOIN_TRANSITION
+from aiogram.utils.markdown import hbold, text
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 from data.permissions import new_user_added
-from aiogram import Router, types, F, Bot
+from loader import bot
 from os import getenv
 from dotenv import load_dotenv
-import aiogram.utils.markdown as fmt
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-import database.requests as rq
-from loader import bot
 
 load_dotenv()
+CHAT_ID = int(getenv("CHAT_ID"))
 
-CHAT_ID = getenv("CHAT_ID")
-TOKEN = getenv("BOT_TOKEN")
 user_router = Router()
-@user_router.message(F.chat.id == int(CHAT_ID))
-async def mes(message: types.Message):
-     print(message.from_user.id)
-     is_user_exist = await rq.check_user(message.from_user.id)
-     if not is_user_exist:
-          await bot.restrict_chat_member(
-               chat_id=message.chat.id,
-               user_id=message.from_user.id,
-               permissions=new_user_added,
-          )
-          # Написать что нужно подписаться на бота
-          builder = InlineKeyboardBuilder()
-          builder.row(types.InlineKeyboardButton(
-               text="Подписаться на бота", url="tg://resolve?domain=GdenaitiBot"))
-          new_msg = await message.answer(
-               fmt.text(fmt.hbold(message.from_user.full_name),
-                    fmt.text(", вы находитесь в режиме чтения. "
-                             "Чтобы писать в чате, пожалуйста, подпишитесь на бота, нажав на кнопку ниже."),
-                    sep=""
-               ), parse_mode="HTML", reply_markup=builder.as_markup(),
-          )
-          await asyncio.sleep(60)
-          try:
-               await new_msg.delete()
-          except Exception as e:
-               pass
 
+@user_router.chat_member(ChatMemberUpdatedFilter(JOIN_TRANSITION))
+async def on_user_joined(event: ChatMemberUpdated, bot: Bot):
+    user = event.from_user
+    chat_id = event.chat.id
+
+    # Ограничиваем пользователя
+    await bot.restrict_chat_member(
+        chat_id=chat_id,
+        user_id=user.id,
+        permissions=new_user_added
+    )
+
+    # Сообщение с кнопкой подписки
+    builder = InlineKeyboardBuilder()
+    builder.row(types.InlineKeyboardButton(
+        text="Подписаться на бота", url="tg://resolve?domain=GdenaitiBot"
+    ))
+
+    try:
+        msg = await bot.send_message(
+            chat_id=chat_id,
+            text=text(
+                hbold(user.full_name),
+                ", вы находитесь в режиме чтения. "
+                "Чтобы писать в чате, подпишитесь на бота, нажав на кнопку ниже."
+            ),
+            parse_mode="HTML",
+            reply_markup=builder.as_markup()
+        )
+        await asyncio.sleep(60)
+        await msg.delete()
+    except Exception:
+        pass
